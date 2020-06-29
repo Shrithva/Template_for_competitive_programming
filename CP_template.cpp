fd837a4091327ea18f91f93c94ac109c9581e6f0
Shrithva_dm <54732889+Shrithva@users.noreply.github.com>
2020-06-29 10:18:47 +0530

    Update CP_template.cpp

--- CP_template.cpp
+++ CP_template.cpp
@@ -4,12 +4,12 @@ using namespace std;
 #define Fo(i,k,n) for(i=k;k<n?i<n:i>n;k<n?i+=1:i-=1)
 #define ll long long
 #define gc getchar_unlocked
-#define si(x)	scanf("%d",&x)
-#define sl(x)	scanf("%lld",&x)
-#define ss(s)	scanf("%s",s)
-#define ps(s)	printf("%s\n",s)
-#define pi(x)	printf("%d\n",x)
-#define pl(x)	printf("%lld\n",x)
+#define si(x) scanf("%d",&x)
+#define sl(x) scanf("%lld",&x)
+#define ss(s) scanf("%s",s)
+#define ps(s) printf("%s\n",s)
+#define pi(x) printf("%d\n",x)
+#define pl(x) printf("%lld\n",x)
 #define deb(x) cout << #x << "=" << x << endl
 #define all(x) x.begin(), x.end()
 #define deb2(x, y) cout << #x << "=" << x << "," << #y << "=" << y << endl
